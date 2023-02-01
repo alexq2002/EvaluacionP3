33e1c38c8124525e369bfffbcb5bff041871063d
alexq2002 <115095396+alexq2002@users.noreply.github.com>
2023-02-01 08:53:23 -0500

    Api y atributos listos

--- EvaluacionP3/MainPage.xaml.cs
+++ EvaluacionP3/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace EvaluacionP3;
+﻿using System.Net;
+
+namespace EvaluacionP3;
 
 public partial class MainPage : ContentPage
 {
@@ -9,16 +11,23 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public async void Button_Clicked(object sender, EventArgs e)
+    {
+        string cadena = Buscador.Text;
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://www.themealdb.com/api/json/v1/1/search.php?f=" + cadena);
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        var client = new HttpClient();
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        HttpResponseMessage response = await client.SendAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<List<ComidaAQ>>(content);
+            ListaDemo.ItemsSource = resultado;
+        }
+    }
 }
 
---
+++ EvaluacionP3/Models/ComidaAQ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionP3.Models
+{
+    internal class ComidaAQ
+    {
+        public string idMeal { get; set; }
+        public string strMeal { get; set; }
+        public object strDrinkAlternate { get; set; }
+        public string strInstructions { get; set; }
+        public string strIngredient1 { get; set; }
+        public string strMeasure1 { get; set; }
+    }
+}
